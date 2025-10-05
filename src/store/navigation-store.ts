442608372818ea64d5055df1a203aa74779d1df6
Ammar Butt <amar.salim.butt@gmail.com>
2025-10-05 07:42:02 +0500

    refactor: remove unused navigation store and Toaster component from Providers

--- src/store/navigation-store.ts
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-export const useNavigationStore = create<{
-  activePageTitle: string;
-  setActivePageTitle: (title: string) => void;
-}>(set => ({
-  activePageTitle: 'Dashboard',
-  setActivePageTitle: (title: string) => set({ activePageTitle: title }),
-}));
